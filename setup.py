e4473fe1e958abcbd53093aef21b1b7a261c1cb6
Angelos Semoglou <a.semoglou@outlook.com>
2025-03-20 15:37:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy==1.26.4",
-        "pandas==2.2.2",
-        "scikit-learn==1.6.1",
-        "scipy==1.14.1",
-        "joblib==1.4.2"
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "joblib>=1.4.2"
     ],
-    python_requires=">=3.11",
+    python_requires=">=3.8",
     description="K-Sil Clustering: Silhouette-Guided Weighted K-Means",
     url="https://github.com/semoglou/ksil",
     author="Angelos Semoglou",
