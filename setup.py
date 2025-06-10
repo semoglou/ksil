0a888c0bf2056ac727a20537cccba5b58e21a08a
Angelos Semoglou <a.semoglou@outlook.com>
2025-06-10 18:08:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="k_silhouette",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -12,9 +12,11 @@ setup(
         "joblib>=1.4.2"
     ],
     python_requires=">=3.8",
-    description="K-Sil Clustering: Silhouette-Guided Instance-Weighted K-Means",
+    description="K-Sil Clustering: A silhouette-guided instance-weighted k-means algorithm that integrates silhouette scores into the clustering process to improve clustering quality.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/semoglou/ksil",
-    author="Angelos Semoglou",
+    author="Aggelos Semoglou",
     author_email="a.semoglou@outlook.com",
     license="MIT"
 )
